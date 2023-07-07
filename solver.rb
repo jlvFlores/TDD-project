6b9bf21bdb8ebd26395d8a01f7294d475ffbdf23
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-07 08:50:13 -0600

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,12 +1,16 @@
 class Solver
   def factorial(int)
     return 1 if [0, 1].include?(int)
+
     int * factorial(int - 1)
   end
+
   def reverse(string)
     return string if string.length <= 1
+
     reverse(string[1..]) + string[0]
   end
+
   def fizzbuzz(int)
     if (int % 3).zero? && (int % 5).zero?
       'fizzbuzz'
