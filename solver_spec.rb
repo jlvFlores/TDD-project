b9790d14dea7215cf36f6e7dd7d17b7bf34ff556
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-06 19:07:44 -0600

    Add unit test for fizzbuzz method

--- solver_spec.rb
+++ solver_spec.rb
@@ -13,6 +13,22 @@ describe Solver do
     it 'reverse method returns a given string backwards' do
       expect(@solver.reverse('hello')).to eq('olleh')
     end
+
+    it 'fizzbuzz method returns fizz when the interger divisible by 3' do
+      expect(@solver.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'fizzbuzz method returns buzz when the interger divisible by 5' do
+      expect(@solver.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'fizzbuzz method returns fizzbuzz when the interger divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'fizzbuzz method returns the interger as a string when its not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(7)).to eq('7')
+    end
   end
 
   context 'test methods when receiving invalid values' do
