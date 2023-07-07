c3755aca1c8e1560f0af4c9fb497e95c0d82f1aa
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-06 19:02:58 -0600

    Add unit test for reverse method

--- solver_spec.rb
+++ solver_spec.rb
@@ -4,9 +4,14 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
+
   context 'test methods when receiving valid values' do
     it 'factorial method return the factorial of the given interger' do
       expect(@solver.factorial(5)).to eq(120)
+    end
+
+    it 'reverse method returns a given string backwards' do
+      expect(@solver.reverse('hello')).to eq('olleh')
     end
   end
 
@@ -15,5 +20,4 @@ describe Solver do
       expect{@solver.factorial(-1)}.to raise_exception(SystemStackError)
     end
   end
-
 end
